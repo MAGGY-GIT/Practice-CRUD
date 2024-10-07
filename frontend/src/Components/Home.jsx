8dd7a7119f1b7353326cb5e3b413ca08e4b7e7c2
MAGGY-GIT <kairumaggie29@gmail.com>
2024-10-07 10:48:23 +0300

    "Updated App.css, Home.jsx, and Navigation.jsx with new styles and code changes"

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
-import '../App.css'
-import studentimage1 from "../static/images/studentimage1.jpg"
-import studentimage2 from "../static/images/studentimage2.jpg"
-import studentimage3 from "../static/images/studentimage3.jpg"
+// import React from 'react'
+// import '../App.css'
+// import { useState } from 'react'
+// import studentimage1 from "../static/images/studentimage1.jpg"
+// import studentimage2 from "../static/images/studentimage2.jpg"
+// import studentimage3 from "../static/images/studentimage3.jpg"
 
-function Home() {
-  return (
-    <div></div>
-  )
-}
+// function Home({slides}) {
+//   const [currentSlide, setCurrentSlide] = useState(0);
+//   // this line creates a variable currentSlide that keeps track of the current/first slide
+//   // setCurrentSlide is used to update the currentSlide to the next slide
 
-export default Home
+//   // the functions below help navigate through the slides
+//   const hanldePrev = () => {
+//     setCurrentSlide(currentSlide - 1);
+//   }
+
+//   const handleNext = () => {
+//     setCurrentSlide(currentSlide + 1);
+//   }
+
+//   return (
+//     <div className='carousel w-full h-full overflow-hidden relative'>
+
+//       <div className='carousel-inner relative w-full h-full'>
+
+//         {slides.map((slide, index) => (
+//           <div key={index} className="carousel-item w-full h-full flex justify-center items-center">
+//             <img src={slide.studentimage1} alt="slide" className="w-full h-full object-cover"/>
+//           </div>
+//         ))}
+        
+//       </div>
+
+      
+//     </div>
+//   )
+// }
+
+// export default Home
 
 
 
--- frontend/src/Components/Navigation.jsx
+++ frontend/src/Components/Navigation.jsx
@@ -7,7 +7,7 @@ import { ImProfile } from "react-icons/im"
 
 const Navigation = () => {
   return (
-    <div className="flex justify-between bg-slate-600 text-white ">
+    <div className="flex justify-between bg-orange-500 text-white ">
       <div className="logo inline-block py-5 mx-10 ">
         <a href="#" className="font-bold flex items-center p-3">
           <FaGraduationCap className=" w-10 h-10 mr-2"/>
@@ -17,23 +17,23 @@ const Navigation = () => {
 
       <div className="links mr-12 mt-7">
         <ul className="flex">
-          <li className=" hover:text-black  hover:bg-white rounded mr-3 p-2">
+          <li className=" hover:text-black  hover:bg-orange-100  rounded mr-3 p-2">
             <a href="#" className="flex items-center">
-              <GoHomeFill className="mr-1 w-5 h-6 "/>
+              <GoHomeFill className="mr-2 w-5 h-6 "/>
               <span className="text-xl">Home</span>
             </a>
           </li>
 
-          <li className=" hover:text-black  hover:bg-white rounded mr-3 p-2">
+          <li className=" hover:text-black  hover:bg-orange-100 rounded mr-3 p-2">
             <a href="#" className="flex items-center">
-              <FaList className="mr-1 w-5 h-6 "/>
+              <FaList className="mr-2 w-5 h-6 "/>
               <span className="text-xl">Students List</span>
             </a>
           </li>
 
-          <li className=" hover:text-black  hover:bg-white rounded mr-3 p-2">
+          <li className=" hover:text-black  hover:bg-orange-100 rounded mr-3 p-2">
             <a href="#" className="flex items-center">
-              <ImProfile className="mr-1 w-5 h-6 "/>
+              <ImProfile className="mr-2 w-5 h-6 "/>
               <span className="text-xl">Student Profiles</span>
             </a>
           </li>
